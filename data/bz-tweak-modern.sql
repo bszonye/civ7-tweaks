3535e68deba5d27c75f2cd1fae8c6e2b3031543c
B. Szonye <bszonye@gmail.com>
2025-08-26 23:35:58 -0700

    remove obsolete wonder tweaks

--- data/bz-tweak-modern.sql
+++
@@ -1,4 +0,0 @@
--- Bridhadeeswarar Temple and Machu Pikchu: ageless adjacency bonuses
-UPDATE Adjacency_YieldChanges SET Age = NULL WHERE ID = 'BrihadeeswararTempleWildcardNavigableHappiness';
-UPDATE Adjacency_YieldChanges SET Age = NULL WHERE ID = 'MachuPikchuWildcardCulture';
-UPDATE Adjacency_YieldChanges SET Age = NULL WHERE ID = 'MachuPikchuWildcardGold';
