4227dc29a08fc947f18a587d5dc69b75ac116114
B. Szonye <bszonye@gmail.com>
2025-12-15 05:17:59 -0800

    improved city-state list

---
+++ ui/diplomacy-actions/bz-panel-player-diplomacy.js
@@ -0,0 +1,84 @@
+
+class bzPlayerDiplomacyActionPanel {
+    static c_prototype;
+    constructor(component) {
+        this.component = component;
+        component.bzComponent = this;
+        this.patchPrototypes(this.component);
+    }
+    patchPrototypes(component) {
+        const c_prototype = Object.getPrototypeOf(component);
+        if (bzPlayerDiplomacyActionPanel.c_prototype == c_prototype) return;
+        // patch component methods
+        const proto = bzPlayerDiplomacyActionPanel.c_prototype = c_prototype;
+        // afterCreateMinorPlayerListItem
+        const afterCreateMinorPlayerListItem = this.afterCreateMinorPlayerListItem;
+        const createMinorPlayerListItem = proto.createMinorPlayerListItem;
+        proto.createMinorPlayerListItem = function(...args) {
+            const item = createMinorPlayerListItem.apply(this, args);
+            args = [item, ...args];
+            return afterCreateMinorPlayerListItem.apply(this.bzComponent, args);
+        }
+    }
+    beforeAttach() {}
+    afterAttach() {}
+    beforeDetach() {}
+    afterDetach() {}
+    afterCreateMinorPlayerListItem(item, player) {
+        const content = item.firstChild;
+        const column = document.createElement("div");
+        column.classList.value =
+            "basis-full shrink flex flex-row flex-row-reverse justify-start items-center";
+        const typeIcon = document.createElement("div");
+        typeIcon.classList.value =
+            "mr-2 size-16 bg-cover bg-black border border-primary rounded-full";
+        const type = GameInfo.Independents
+            .find(i => player.civilizationAdjective == i.CityStateName)
+            ?.CityStateType.toLowerCase();
+        const color =
+            type == "militaristic" ? "#af1b1c" :
+            type == "scientific" ? "#4d7c96" :
+            type == "economic" ? "#ffd553" :
+            type == "cultural" ? "#892bb3" :
+            type == "diplomatic" ? "#255be4" :
+            type == "expansionist" ? "#00a717" :
+            "#af1b1c";
+        typeIcon.style.backgroundImage = `url('blp:bonustype_${type}.png')`;
+        typeIcon.style.fxsBackgroundImageTint = color;
+        column.appendChild(typeIcon);
+        content.appendChild(column);
+        // befriending status
+        const observer = Players.get(GameContext.localObserverID);
+        const diplomacy = observer.Diplomacy;
+        const befriendType = DiplomacyActionTypes.DIPLOMACY_ACTION_GIVE_INFLUENCE_TOKEN;
+        const actions = Game.Diplomacy.getPlayerEvents(player.id)
+            .filter(act => act.actionType == befriendType);
+        const befriending = [];
+        for (const act of actions) {
+            const target = Players.get(act.targetPlayer);
+            if (target.Influence?.hasSuzerain) continue;
+            const player = Configuration.getPlayer(act.initialPlayer);
+            const cdata = Game.Diplomacy.getCompletionData(act.uniqueID);
+            const turns = cdata.turnsToCompletion;
+            const order = player.id <= observer ? player.id + 1000 : player.id;
+            befriending.push({ target, player, turns, order });
+        }
+        befriending.sort((a, b) => a.turns - b.turns || a.order - b.order);
+        for (const friend of befriending) {
+            const friendIcon = document.createElement("leader-icon");
+            friendIcon.classList.add("mr-2", "mt-2", "size-13");
+            if (diplomacy.hasMet(friend.player.id) || friend.player.id == observer.id) {
+                friendIcon.setAttribute("leader", friend.player.leaderTypeName);
+                friendIcon.setAttribute(
+                    "bg-color",
+                    UI.Player.getPrimaryColorValueAsString(friend.player.id)
+                );
+            } else {
+                friendIcon.setAttribute("leader", "LEADER_UNMET");
+            }
+            column.appendChild(friendIcon);
+        }
+        return item;
+    }
+}
+Controls.decorate("panel-player-diplomacy-actions", (c) => new bzPlayerDiplomacyActionPanel(c));
