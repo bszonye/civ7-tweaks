3c791a112dfa560ceedbd72cf5c654176a8b6994
B. Szonye <bszonye@gmail.com>
2026-03-05 00:33:19 -0800

    mod list formatting

--- ui/shell/bz-tweak-shell.js
+++ ui/shell/bz-tweak-shell.js
@@ -14,17 +14,19 @@ const BZ_HEAD_STYLE = [
 .additional-content-mods fxs-vslot.w-1\\/4 {
     margin-top: -1.1111111111rem;
 }
-.additional-content-mods .bz-odd-row {
-    background: linear-gradient(90deg, #333640 0%, #33364000 100%);
-}
 .additional-content-mods .bz-even-row, .additional-content-mods .bz-odd-row {
-    border-radius: 1em 0 0 1em;
+    /* border-radius: 1em 0 0 1em; */
+}
+.additional-content-mods .bz-odd-row {
+    background: linear-gradient(90deg, #333640 0%, #33364066 100%);
 }
 .additional-content-mods .bz-even-row:focus, .additional-content-mods .bz-odd-row:focus,
 .additional-content-mods .bz-even-row:hover, .additional-content-mods .bz-odd-row:hover,
 .additional-content-mods .bz-even-row.pressed,
 .additional-content-mods .bz-odd-row.pressed {
-    background: linear-gradient(90deg, #8c7e62 0%, #8c7e6200 100%);
+    background: linear-gradient(90deg, #8c7e62 0%, #8c7e6266 100%);
+    color: #e5e5e5;
+    text-shadow: 0 0.0555555556rem 0.1111111111rem black;
 }
 `,
 ];
