3f181ec3959b30a52b0779615cf5e217a2233921
B. Szonye <bszonye@gmail.com>
2026-03-04 18:12:05 -0800

    Add-ons panel tweaks

--- ui/shell/bz-tweak-shell.js
+++ ui/shell/bz-tweak-shell.js
@@ -1,8 +1,15 @@
+import { ScreenExtras } from '/core/ui/shell/extras/screen-extras.js';
+
 const BZ_HEAD_STYLE = [
 `
+.additional-content-back-button,
 .extras-item-benchmark-graphics,
 .extras-item-benchmark-ai {
     display: none;
+}
+.mod-list-scrollable .-right-1\\.5 {
+    right: auto;
+    left: -0.3333333333rem;
 }
 `,
 ];
@@ -11,3 +18,16 @@ BZ_HEAD_STYLE.map(style => {
     e.textContent = style;
     document.head.appendChild(e);
 });
+
+const SE_onEngineInput = ScreenExtras.prototype.onEngineInput;
+ScreenExtras.prototype.onEngineInput = function(...args) {
+    SE_onEngineInput.apply(this, args);
+    const [e] = args;
+    if (e.detail.status != InputActionStatuses.FINISH) return;
+    if (e.defaultPrevented) return;
+    if (e.detail.name == "mousebutton-right") {
+        this.close();
+        e.stopPropagation();
+        e.preventDefault();
+    }
+}
