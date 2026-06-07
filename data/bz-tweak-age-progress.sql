f8ad2aea182d9211d3da04ca5dd9313b53ed85db
B. Szonye <bszonye@gmail.com>
2026-06-06 17:33:15 -0700

    undo age length changes

--- data/bz-tweak-age-progress.sql
+++ data/bz-tweak-age-progress.sql
@@ -1,16 +1,18 @@
 -- no milestone progress
-UPDATE AgeProgressionEvents SET Points=0 WHERE AgeProgressionEventType="AGE_PROGRESSION_PLAYER_MILESTONE_1";
-UPDATE AgeProgressionEvents SET Points=0 WHERE AgeProgressionEventType="AGE_PROGRESSION_PLAYER_MILESTONE_2";
-UPDATE AgeProgressionEvents SET Points=0 WHERE AgeProgressionEventType="AGE_PROGRESSION_PLAYER_MILESTONE_3";
+-- UPDATE AgeProgressionEvents SET Points=0 WHERE AgeProgressionEventType="AGE_PROGRESSION_PLAYER_MILESTONE_1";
+-- UPDATE AgeProgressionEvents SET Points=0 WHERE AgeProgressionEventType="AGE_PROGRESSION_PLAYER_MILESTONE_2";
+-- UPDATE AgeProgressionEvents SET Points=0 WHERE AgeProgressionEventType="AGE_PROGRESSION_PLAYER_MILESTONE_3";
 
 -- no player elimination progress
-UPDATE AgeProgressionEvents SET Points=0 WHERE AgeProgressionEventType="AGE_PROGRESSION_PLAYER_ELIMINATED";
-UPDATE AgeProgressionEventMapSizeOverrides SET Points=0 WHERE AgeProgressionEventType="AGE_PROGRESSION_PLAYER_ELIMINATED";
+-- UPDATE AgeProgressionEvents SET Points=0 WHERE AgeProgressionEventType="AGE_PROGRESSION_PLAYER_ELIMINATED";
+-- UPDATE AgeProgressionEventMapSizeOverrides SET Points=0 WHERE AgeProgressionEventType="AGE_PROGRESSION_PLAYER_ELIMINATED";
 
 -- adjust age lengths
-UPDATE AgeProgressions SET MaxPoints_Abbreviated=120, MaxPoints_Standard=150, MaxPoints_Long=180 WHERE AgeProgressionType="AGE_PROGRESSION_ANTIQUITY_AGE_TIMER";
-UPDATE AgeProgressions SET MaxPoints_Abbreviated=90, MaxPoints_Standard=120, MaxPoints_Long=150 WHERE AgeProgressionType="AGE_PROGRESSION_EXPLORATION_AGE_TIMER";
-UPDATE AgeProgressions SET MaxPoints_Abbreviated=90, MaxPoints_Standard=120, MaxPoints_Long=150 WHERE AgeProgressionType="AGE_PROGRESSION_MODERN_AGE_TIMER";
+-- UPDATE AgeProgressions SET MaxPoints_Abbreviated=120, MaxPoints_Standard=150, MaxPoints_Long=180 WHERE AgeProgressionType="AGE_PROGRESSION_ANTIQUITY_AGE_TIMER";
+-- UPDATE AgeProgressions SET MaxPoints_Abbreviated=90, MaxPoints_Standard=120, MaxPoints_Long=150 WHERE AgeProgressionType="AGE_PROGRESSION_EXPLORATION_AGE_TIMER";
+-- UPDATE AgeProgressions SET MaxPoints_Abbreviated=90, MaxPoints_Standard=120, MaxPoints_Long=150 WHERE AgeProgressionType="AGE_PROGRESSION_MODERN_AGE_TIMER";
+
+-- are these important?
 
 -- UPDATE GlobalParameters SET Value=120 WHERE Name="GAME_AGE_LENGTH_ABBREVIATED" AND EXISTS (SELECT Age FROM AgeGrowthBalances WHERE Age="AGE_ANTIQUITY");
 -- UPDATE GlobalParameters SET Value=90 WHERE Name="GAME_AGE_LENGTH_ABBREVIATED" AND EXISTS (SELECT Age FROM AgeGrowthBalances WHERE Age="AGE_EXPLORATION");
