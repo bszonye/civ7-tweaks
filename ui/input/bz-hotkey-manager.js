248de80986f06475a3da2d77a10baa45f50b1787
B. Szonye <bszonye@gmail.com>
2026-07-28 02:06:38 -0700

    add hotkey hooks for Minor Powers panel

---
+++ ui/input/bz-hotkey-manager.js
@@ -0,0 +1,17 @@
+import HotkeyManager from '/core/ui/input/hotkey-manager.js';
+
+const HM_handleInput = HotkeyManager.handleInput;
+HotkeyManager.handleInput = function(...args) {
+    const [inputEvent] = args;
+    const status = inputEvent.detail.status;
+    if (status == InputActionStatuses.FINISH) {
+        const name = inputEvent.detail.name;
+        switch (name) {
+            case "bz-open-minor-powers":
+                UI.reloadUI();  // TODO
+                return false;
+        }
+    }
+    // default handler
+    return HM_handleInput.apply(this, args);
+}
